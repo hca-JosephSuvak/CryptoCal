2c5ae038af79c05cd93fcf66b64368b991a54da3
walterego <walter.scimone@gmail.com>
2021-03-24 01:52:30 -0400

    fixed API URL and added a userSearch variable

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,10 @@
+// creating a global variable to be the future userInputText...
+var userSearch = "ethereum"
+
 // creating a function that fetches the Crypto Exchanges list API...
 var searchCrypto = function() {
   
-    var apiUrl = "https://api.coingecko.com/api/v3/exchanges/list"
+    var apiUrl = ("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=" + userSearch);
   
     // fetching the API and making sure the async works with console log...
     fetch(apiUrl).then(function(response) {
@@ -9,8 +12,9 @@ var searchCrypto = function() {
       response.json().then(function(data) {
         console.log(JSON.stringify(data));
         console.log("The fetch works!");
+       
       });
     });
   };
   
-  searchCrypto();
+  searchCrypto(userSearch);
